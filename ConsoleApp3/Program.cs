4c861b1978c5fd04b0157de0783d3d84476ead1b
MartinO24 <martin.ollinen@gmail.com>
2018-01-26 13:07:42 +0100

    Model Building

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Building
+    {
+        public string Address { get; set; }
+        public int FloorLevels { get; set; }
+    }
+}
